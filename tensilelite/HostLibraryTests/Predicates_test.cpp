06783c34cb37db36cf492d343b33969e5dfb0e17
wmahmood <wmahmood@amd.com>
2024-08-15 16:22:55 -0500

    Enabling Predicate test

--- tensilelite/HostLibraryTests/Predicates_test.cpp
+++ tensilelite/HostLibraryTests/Predicates_test.cpp
@@ -32,13 +32,13 @@ TEST(Predicates, ArithmeticIntensity)
 {
     using namespace Tensile;
 
-    ContractionProblem a = ContractionProblem::GEMM(
+    ContractionProblemGemm a = ContractionProblemGemm::GEMM(
         false, true, 1000, 1500, 500, 2000, 2000, 2000, 3.0, false, 1); // 88.4
-    ContractionProblem b = ContractionProblem::GEMM(
+    ContractionProblemGemm b = ContractionProblemGemm::GEMM(
         false, true, 500, 1000, 1000, 2000, 2000, 2000, 0.0, false, 5); // 125
-    ContractionProblem c = ContractionProblem::GEMM(
+    ContractionProblemGemm c = ContractionProblemGemm::GEMM(
         false, true, 2000, 100, 2000, 2000, 2000, 2000, 1.0, false, 10); // 43.5
-    ContractionProblem d = ContractionProblem::GEMM(
+    ContractionProblemGemm d = ContractionProblemGemm::GEMM(
         false, true, 2000, 2000, 450, 2000, 2000, 2000, 2.0, false, 1); // 92.04
 
     auto pg1 = std::make_shared<Predicates::Contraction::AIGreaterThanEqual>(100);
